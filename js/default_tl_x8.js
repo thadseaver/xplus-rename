291ccbda1583398167dfd0861230621757f31af2
Thad Seaver <twseaver88@yahoo.com>
2014-12-08 18:14:45 -0500

    Added files for Mastercam X8

---
+++ js/default_tl_x8.js
@@ -0,0 +1,46 @@
+/*
+Purpose				: Changes the column names in the HTML X+ Tool List
+Mastercam version	: All variations of X8
+Date				: Dec 2014
+Filename			: default_tl_x8.js
+Author				: Thad Seaver
+Email				: thad@thadseaver.com
+
+X+ Tool List can be downloaded from the German Mastercam reseller GMCCS at:
+http://gmccs.de/?page=downloads.php&dl_grp=8&pt=X%2B+Tool+Downloads
+*/
+
+window.onload = function(){
+	
+		var colNames = document.getElementsByTagName('th');
+
+		for (var i = 0; i < colNames.length; i++) {
+			
+			//colNames[i].innerHTML = colNames[i].innerHTML.replace('Assembly name', 'Your text here');
+			//colNames[i].innerHTML = colNames[i].innerHTML.replace('Coolant', 'Your text here');
+			//colNames[i].innerHTML = colNames[i].innerHTML.replace('Cycle time', 'Your text here');
+			//colNames[i].innerHTML = colNames[i].innerHTML.replace('Feedrate XY', 'Your text here');
+			//colNames[i].innerHTML = colNames[i].innerHTML.replace('Feedrate Z+', 'Your text here');
+			//colNames[i].innerHTML = colNames[i].innerHTML.replace('Feedrate Z-', 'Your text here');
+			//colNames[i].innerHTML = colNames[i].innerHTML.replace('Holder (old)', 'Your text here');
+			//colNames[i].innerHTML = colNames[i].innerHTML.replace('Holder (new)', 'Your text here');
+			//colNames[i].innerHTML = colNames[i].innerHTML.replace('Insert', 'Your text here');
+			//colNames[i].innerHTML = colNames[i].innerHTML.replace('Spindle speed', 'Your text here');
+			//colNames[i].innerHTML = colNames[i].innerHTML.replace('Tl #', 'Your text here');
+			//colNames[i].innerHTML = colNames[i].innerHTML.replace('Tl BMP', 'Your text here');
+			//colNames[i].innerHTML = colNames[i].innerHTML.replace('Tl corner rad', 'Your text here');
+			//colNames[i].innerHTML = colNames[i].innerHTML.replace('Tl Cutlength', 'Your text here');
+			//colNames[i].innerHTML = colNames[i].innerHTML.replace('Tl D#', 'Your text here');
+			//colNames[i].innerHTML = colNames[i].innerHTML.replace('Tl diam.', 'Your text here');
+			//colNames[i].innerHTML = colNames[i].innerHTML.replace('Tl flutes', 'Your text here');
+			//colNames[i].innerHTML = colNames[i].innerHTML.replace('Tl L#', 'Your text here');
+			//colNames[i].innerHTML = colNames[i].innerHTML.replace('Tl length', 'Your text here');
+			//colNames[i].innerHTML = colNames[i].innerHTML.replace('Tl shldr length', 'Your text here');
+			//colNames[i].innerHTML = colNames[i].innerHTML.replace('Tl type', 'Your text here');
+			//colNames[i].innerHTML = colNames[i].innerHTML.replace('Tl name', 'Your text here');
+			//colNames[i].innerHTML = colNames[i].innerHTML.replace('Tl manufactor', 'Your text here');
+			//colNames[i].innerHTML = colNames[i].innerHTML.replace('Z-Max', 'Your text here');
+			//colNames[i].innerHTML = colNames[i].innerHTML.replace('Z-Min', 'Your text here');
+		
+		}		
+}; /* end script */
