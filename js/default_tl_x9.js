cecfdbf6e81846cd1abbd4829e8ec1726132b443
Thad Seaver <twseaver88@yahoo.com>
2015-08-21 19:50:25 -0400

    Added files for X9

---
+++ js/default_tl_x9.js
@@ -0,0 +1,46 @@
+/*
+Purpose				: Changes the column names in the HTML X+ Tool List
+Mastercam version	: All variations of X9
+Date				: Aug 2015
+Filename			: default_tl_x9.js
+Author				: Thad Seaver
+Email				: thad@thadseaver.com
+
+X+ can be downloaded from the German Mastercam reseller GMCCS at:
+http://gmccs.de/downloads_x+.php
+*/
+
+window.onload = function(){
+	
+		var colNames = document.getElementsByTagName('th');
+
+		for (var i = 0; i < colNames.length; i++) {
+			
+			//colNames[i].innerHTML = colNames[i].innerHTML.replace('Assembly name', 'Your text here');
+			//colNames[i].innerHTML = colNames[i].innerHTML.replace('Coolant', 'Your text here');
+			//colNames[i].innerHTML = colNames[i].innerHTML.replace('Cycle time', 'Your text here');
+			//colNames[i].innerHTML = colNames[i].innerHTML.replace('Feedrate XY', 'Your text here');
+			//colNames[i].innerHTML = colNames[i].innerHTML.replace('Feedrate Z+', 'Your text here');
+			//colNames[i].innerHTML = colNames[i].innerHTML.replace('Feedrate Z-', 'Your text here');
+			//colNames[i].innerHTML = colNames[i].innerHTML.replace('Holder (old)', 'Your text here');
+			//colNames[i].innerHTML = colNames[i].innerHTML.replace('Holder (new)', 'Your text here');
+			//colNames[i].innerHTML = colNames[i].innerHTML.replace('Insert', 'Your text here');
+			//colNames[i].innerHTML = colNames[i].innerHTML.replace('Spindle speed', 'Your text here');
+			//colNames[i].innerHTML = colNames[i].innerHTML.replace('Tl #', 'Your text here');
+			//colNames[i].innerHTML = colNames[i].innerHTML.replace('Tl BMP', 'Your text here');
+			//colNames[i].innerHTML = colNames[i].innerHTML.replace('Tl corner rad', 'Your text here');
+			//colNames[i].innerHTML = colNames[i].innerHTML.replace('Tl Cutlength', 'Your text here');
+			//colNames[i].innerHTML = colNames[i].innerHTML.replace('Tl D#', 'Your text here');
+			//colNames[i].innerHTML = colNames[i].innerHTML.replace('Tl diam.', 'Your text here');
+			//colNames[i].innerHTML = colNames[i].innerHTML.replace('Tl flutes', 'Your text here');
+			//colNames[i].innerHTML = colNames[i].innerHTML.replace('Tl L#', 'Your text here');
+			//colNames[i].innerHTML = colNames[i].innerHTML.replace('Tl length', 'Your text here');
+			//colNames[i].innerHTML = colNames[i].innerHTML.replace('Tl shldr length', 'Your text here');
+			//colNames[i].innerHTML = colNames[i].innerHTML.replace('Tl type', 'Your text here');
+			//colNames[i].innerHTML = colNames[i].innerHTML.replace('Tl name', 'Your text here');
+			//colNames[i].innerHTML = colNames[i].innerHTML.replace('Tl manufactor', 'Your text here');
+			//colNames[i].innerHTML = colNames[i].innerHTML.replace('Z-Max', 'Your text here');
+			//colNames[i].innerHTML = colNames[i].innerHTML.replace('Z-Min', 'Your text here');
+		
+		}		
+}; /* end script */
---
+++ js/default_xss_x9.js
@@ -0,0 +1,71 @@
+/*
+Purpose				: Changes the column names in the HTML X+ Setup Sheet
+Mastercam version	: All variations of X9
+Date				: Aug 2015
+Filename			: default_xss_x9.js
+Author				: Thad Seaver
+Email				: thad@thadseaver.com
+
+X+ can be downloaded from the German Mastercam reseller GMCCS at:
+http://gmccs.de/downloads_x+.php
+*/
+
+window.onload = function(){
+	
+		var colNames = document.getElementsByTagName('th');
+
+		for (var i = 0; i < colNames.length; i++) {
+
+			//colNames[i].innerHTML = colNames[i].innerHTML.replace('Assembly name', 'Your text here');
+			//colNames[i].innerHTML = colNames[i].innerHTML.replace('Comment', 'Your text here');
+			//colNames[i].innerHTML = colNames[i].innerHTML.replace('Coolant', 'Your text here');
+			//colNames[i].innerHTML = colNames[i].innerHTML.replace('Cut angle', 'Your text here');
+			//colNames[i].innerHTML = colNames[i].innerHTML.replace('Cutter comp', 'Your text here');
+			//colNames[i].innerHTML = colNames[i].innerHTML.replace('Cycle time', 'Your text here');
+			//colNames[i].innerHTML = colNames[i].innerHTML.replace('Feed plane', 'Your text here');
+			//colNames[i].innerHTML = colNames[i].innerHTML.replace('Feedrate XY', 'Your text here');
+			//colNames[i].innerHTML = colNames[i].innerHTML.replace('Feedrate Z-', 'Your text here');
+			//colNames[i].innerHTML = colNames[i].innerHTML.replace('Feedrate Z+', 'Your text here');
+			//colNames[i].innerHTML = colNames[i].innerHTML.replace('Filter', 'Your text here');
+			//colNames[i].innerHTML = colNames[i].innerHTML.replace('Filter ratio', 'Your text here');
+			//colNames[i].innerHTML = colNames[i].innerHTML.replace('Groupname', 'Your text here');
+			//colNames[i].innerHTML = colNames[i].innerHTML.replace('Holder (new)', 'Your text here');
+			//colNames[i].innerHTML = colNames[i].innerHTML.replace('Holder (old)', 'Your text here');
+			//colNames[i].innerHTML = colNames[i].innerHTML.replace('Initheight', 'Your text here');
+			//colNames[i].innerHTML = colNames[i].innerHTML.replace('Insert', 'Your text here');
+			//colNames[i].innerHTML = colNames[i].innerHTML.replace('Op No.', 'Your text here');
+			//colNames[i].innerHTML = colNames[i].innerHTML.replace('Operation type', 'Your text here');
+			//colNames[i].innerHTML = colNames[i].innerHTML.replace('Overall tolerance', 'Your text here');
+			//colNames[i].innerHTML = colNames[i].innerHTML.replace('Prog No.', 'Your text here');
+			//colNames[i].innerHTML = colNames[i].innerHTML.replace('Progname', 'Your text here');
+			//colNames[i].innerHTML = colNames[i].innerHTML.replace('Retr. plane', 'Your text here');
+			//colNames[i].innerHTML = colNames[i].innerHTML.replace('Spindle speed', 'Your text here');
+			//colNames[i].innerHTML = colNames[i].innerHTML.replace('Stepdown', 'Your text here');
+			//colNames[i].innerHTML = colNames[i].innerHTML.replace('Stepover', 'Your text here');
+			//colNames[i].innerHTML = colNames[i].innerHTML.replace('Stock XY', 'Your text here');
+			//colNames[i].innerHTML = colNames[i].innerHTML.replace('Stock Z', 'Your text here');
+			//colNames[i].innerHTML = colNames[i].innerHTML.replace('Tl #', 'Your text here');
+			//colNames[i].innerHTML = colNames[i].innerHTML.replace('Tl BMP', 'Your text here');
+			//colNames[i].innerHTML = colNames[i].innerHTML.replace('Tl corner rad', 'Your text here');
+			//colNames[i].innerHTML = colNames[i].innerHTML.replace('Tl Cutlength', 'Your text here');
+			//colNames[i].innerHTML = colNames[i].innerHTML.replace('Tl D#', 'Your text here');
+			//colNames[i].innerHTML = colNames[i].innerHTML.replace('Tl diam.', 'Your text here');
+			//colNames[i].innerHTML = colNames[i].innerHTML.replace('Tl flutes', 'Your text here');
+			//colNames[i].innerHTML = colNames[i].innerHTML.replace('Tl L#', 'Your text here');
+			//colNames[i].innerHTML = colNames[i].innerHTML.replace('Tl length', 'Your text here');
+			//colNames[i].innerHTML = colNames[i].innerHTML.replace('Tl manufactor', 'Your text here');
+			//colNames[i].innerHTML = colNames[i].innerHTML.replace('Tl name', 'Your text here');
+			//colNames[i].innerHTML = colNames[i].innerHTML.replace('Tl shldr length', 'Your text here');
+			//colNames[i].innerHTML = colNames[i].innerHTML.replace('Tl tip', 'Your text here');
+			//colNames[i].innerHTML = colNames[i].innerHTML.replace('Tl type', 'Your text here');
+			//colNames[i].innerHTML = colNames[i].innerHTML.replace('Tolerance', 'Your text here');
+			//colNames[i].innerHTML = colNames[i].innerHTML.replace('Top of stock', 'Your text here');
+			//colNames[i].innerHTML = colNames[i].innerHTML.replace('Tplane comment', 'Your text here');
+			//colNames[i].innerHTML = colNames[i].innerHTML.replace('Tplane name', 'Your text here');
+			//colNames[i].innerHTML = colNames[i].innerHTML.replace('Tplane No.', 'Your text here');
+			//colNames[i].innerHTML = colNames[i].innerHTML.replace('Workoffset No.', 'Your text here');
+			//colNames[i].innerHTML = colNames[i].innerHTML.replace('Z-Min', 'Your text here');
+			//colNames[i].innerHTML = colNames[i].innerHTML.replace('Z-Max', 'Your text here');
+		
+		}		
+}; /* end script */
